0a7c0550707cc5d0edb96c8dbbc22f81f86a3d18
Uswah-Saeed <uswah.saeed.malik@gmail.com>
2021-09-28 23:32:09 +0500

    Toggle-the-modes

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+function toggleClass() {
+
+    const body = document.querySelector('body');
+    body.classList.toggle('light');
+
+}
+
 const deg = 6;
 const hr = document.querySelector('#hr');
 const mn = document.querySelector('#mn');
